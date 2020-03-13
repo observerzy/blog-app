371511a52c87eb89505631575d3923ab7a89ce1b
observerzy <695752760@qq.com>
2020-03-14 01:49:22 +0800

    after eject

---
+++ src/home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Home: React.SFC = () => {
+    return <div className={styles.home}>home</div>;
+};
+
+export default Home;
