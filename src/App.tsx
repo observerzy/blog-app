52526b6390096fe21a165e1fcccb1dfa8fba0fa2
observerzy <695752760@qq.com>
2020-03-14 03:19:25 +0800

    use sass

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./home";
+import Login from "./login";
 
 function App() {
     return (
@@ -20,6 +21,7 @@ function App() {
                     Learn React
                 </a>
                 <Home></Home>
+                <Login></Login>
             </header>
         </div>
     );
