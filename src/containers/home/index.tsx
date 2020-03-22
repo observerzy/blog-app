a379c01bf5fe7bb2383c99f9760109b85051d222
observerzy <695752760@qq.com>
2020-03-23 02:04:38 +0800

    feat 添加nav

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import style from './index.module.scss';
 import marked from 'marked';
-import { Input } from 'antd';
+import { Input, Tooltip } from 'antd';
 const Home: React.SFC = () => {
   const markedRef = React.useRef<HTMLDivElement>(null);
+  const [searchValue, setSearchValue] = React.useState<string>('');
+  const [isCurrent, setIsCurrent] = React.useState<number>(0);
+  const navList = [
+    { name: '首页', uri: '#' },
+    { name: '关于', uri: '#' }
+  ];
   React.useEffect(() => {
     markedRef.current.innerHTML = marked(
       '# Marked in browser\n\nRendered by **marked**.'
@@ -13,22 +19,46 @@ const Home: React.SFC = () => {
     <div className={style.main}>
       <div className={style.header}>
         <div className={style.headerContainer}>
-          <div
-            onClick={() => {
-              window.location.reload();
-            }}
-            className={style.title}
-          >
-            CodeWorld 0和1的世界
+          <div className={style.title}>
+            <span
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              CodeWorld 0和1的世界
+            </span>
           </div>
           <div className={style.mark}>——记录我的成长历程</div>
+          <div className={style.navMenu}>
+            {navList.map((val, index) => (
+              <>
+                <a
+                  onClick={() => {
+                    setIsCurrent(index);
+                  }}
+                  href={val.uri}
+                  className={isCurrent === index ? style.current : null}
+                >
+                  {val.name}
+                </a>
+              </>
+            ))}
+          </div>
         </div>
         <div className={style.search}>
-          <Input.Search
-            style={{ borderRadius: '3px' }}
-            placeholder="输入关键字搜索"
-            onSearch={value => console.log(value)}
-          />
+          <Tooltip placement="topLeft" title="o(￣▽￣)ｄ">
+            <Input.Search
+              style={{ borderRadius: '3px' }}
+              placeholder="输入关键字搜索"
+              value={searchValue}
+              onChange={e => {
+                setSearchValue(e.target.value);
+              }}
+              onSearch={() => {
+                setSearchValue('');
+              }}
+            />
+          </Tooltip>
         </div>
       </div>
       <div className={style.body}>
