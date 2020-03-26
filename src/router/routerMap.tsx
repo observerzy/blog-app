96ff81a90a5e2487321c6e08ff19703f1fb32eee
observerzy <695752760@qq.com>
2020-03-27 00:42:02 +0800

    format

--- src/router/routerMap.tsx
+++ src/router/routerMap.tsx
@@ -1,42 +1,42 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
 const Test = lazy(() =>
-  import(/*webpackChunkName:"test"*/ '../containers/test')
+    import(/*webpackChunkName:"test"*/ "../containers/test")
 );
 const Page = lazy(() =>
-  import(/*webpackChunkName:"page"*/ '../containers/page')
+    import(/*webpackChunkName:"page"*/ "../containers/page")
 );
 const Register = lazy(() =>
-  import(/*webpackChunkName:"register"*/ '../containers/register')
+    import(/*webpackChunkName:"register"*/ "../containers/register")
 );
 const Home = lazy(() =>
-  import(/*webpackChunkName:"home"*/ '../containers/home')
+    import(/*webpackChunkName:"home"*/ "../containers/home")
 );
 const Writing = lazy(() =>
-  import(/*webpackChunkName:"writing"*/ '@/containers/writing')
+    import(/*webpackChunkName:"writing"*/ "@/containers/writing")
 );
 
 const routerMap = [
-  {
-    path: '/',
-    component: Home
-  },
-  {
-    path: '/test',
-    component: Test
-  },
-  {
-    path: '/page',
-    component: Page
-  },
-  {
-    path: '/register',
-    component: Register
-  },
-  {
-    path: '/writing',
-    component: Writing
-  }
+    {
+        path: "/",
+        component: Home
+    },
+    {
+        path: "/test",
+        component: Test
+    },
+    {
+        path: "/page",
+        component: Page
+    },
+    {
+        path: "/register",
+        component: Register
+    },
+    {
+        path: "/writing",
+        component: Writing
+    }
 ];
 
 export default routerMap;
