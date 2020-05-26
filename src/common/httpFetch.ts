6d9bec7f2e49c3518e3c6a4ff56b01f7afa736e3
observerzy <695752760@qq.com>
2020-05-26 23:59:26 +0800

    feat:添加并封装fetch请求

--- src/common/httpFetch.ts
+++ src/common/httpFetch.ts
@@ -1,3 +1,48 @@
-class httpFetch {}
+//返参可以有多种情况，此处是json
 
-export default new httpFetch();
+// enum ContentType {
+//     json = "application/json;charset=UTF-8",
+//     formData = "multipart/form-data;charset=UTF-8"
+// }
+interface Header {
+    'Content-Type'?: string;
+    [propName: string]: any; //索引签名
+}
+interface Request {
+    method: 'POST' | 'GET';
+    headers: Header;
+    body: any;
+}
+
+interface HttpFetch {
+    postFetch<R>(url: string, params: FormData): Promise<R>;
+}
+class HttpRequest implements HttpFetch {
+    public postFetch<R>(url: string, params: FormData): Promise<R> {
+        let options: Request = {
+            method: 'POST',
+            headers: {},
+            body: {}
+        };
+        url = `/app${url}?app_login_token=${localStorage.getItem(
+            'app_login_token'
+        )}`;
+        return fetch(url, options)
+            .then(resp => {
+                //服务错误400、500
+                if (!resp.ok) {
+                    throw new Error('保存失败');
+                }
+                return resp.json();
+            })
+            .then(data => {
+                //参数错误
+                if (data.header.retCode === '1') {
+                    throw new Error(data.header.errotMsg || '保存失败');
+                }
+                return data;
+            });
+    }
+}
+
+export default new HttpRequest();
